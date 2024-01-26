dfc2051ee9bd25ad90c2c3e09f7c0e26869a6dc3
tianyi.zhang <tianyi.zhang@10.200.14.227>
2024-01-26 20:34:45 +0800

    add:initial

---
+++ nvidia-ros-docker/docker_run.sh
@@ -0,0 +1,30 @@
+CONTAINER_NAME="noetic_docker"
+IMAGE_NAME="hf/nvidia-ros-noetic"
+SHARED_DIR="/home/tianyi.zhang/ws/caric_dku/cwc_docker/shared_dir"
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+docker run -it \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="$XSOCK:$XSOCK:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --net=host \
+    --gpus all \
+    --volume="$SHARED_DIR:$SHARED_DIR" \
+    --name=$CONTAINER_NAME \
+    $IMAGE_NAME \
+    bash
+
